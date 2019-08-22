54120bef9e766801a17db42d0ca679c5f43beb96
Arvind Kumar <arvind@enkrypt.in>
2019-08-22 17:15:11 +0530

    Finalized API structure and polling logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
 import Koa from 'koa';
 import SI from 'systeminformation';
+import cuid from 'cuid';
+
+const pollInterval = 2500;
+const port = 3000;
 
 const app = new Koa();
 
+let iterID = '';
 let system: SI.Systeminformation.SystemData;
 let cpu: SI.Systeminformation.CpuData;
 let os: SI.Systeminformation.OsData;
@@ -12,12 +17,12 @@ let cpuLoad: SI.Systeminformation.CurrentLoadData;
 let mem: SI.Systeminformation.MemData;
 let disk: SI.Systeminformation.FsSizeData[];
 let processes: SI.Systeminformation.ProcessesData;
-let disksIO: SI.Systeminformation.DisksIoData;
+let fsStats: SI.Systeminformation.FsStatsData;
 let network: SI.Systeminformation.NetworkStatsData[];
 
 app.use(async ctx => {
-    ctx.body = {
-        meta: {
+    if (iterID) {
+        const meta = {
             motherboard: {
                 manufacturer: system.manufacturer,
                 model: system.model
@@ -31,33 +36,49 @@ app.use(async ctx => {
                 distro: os.distro,
                 hostname: os.hostname
             }
-        },
-        uptime: SI.time().uptime,
-        cpuTemp: Math.max(...cpuTemp.cores),
-        cpuLoad: cpuLoad.currentload,
-        mem: {
-            total: Math.round(mem.total / 1024 / 1024),
-            active: Math.round(mem.active / 1024 / 1024),
-            swaptotal: Math.round(mem.swaptotal / 1024 / 1024),
-            swapused: Math.round(mem.swapused / 1024 / 1024)
-        },
-        processes: processes.all,
-        disk: disk.map(disk => ({
-            size: Math.round(+disk.size / 1024 / 1024 / 1024),
-            used: Math.round(+disk.used / 1024 / 1024 / 1024)
-        })),
-        disksIO: disksIO,
-        network: network
-    };
+        };
+        ctx.body = {
+            iterID: iterID,
+            uptime: SI.time().uptime,
+            cpuTemp: Math.max(...cpuTemp.cores),
+            cpuLoad: Math.round(cpuLoad.currentload),
+            mem: {
+                total: Math.round(mem.total / 1024 / 1024),
+                active: Math.round(mem.active / 1024 / 1024),
+                swaptotal: Math.round(mem.swaptotal / 1024 / 1024),
+                swapused: Math.round(mem.swapused / 1024 / 1024)
+            },
+            processes: processes.all,
+            disk: disk.map(disk => ({
+                size: Math.round(+disk.size / 1024 / 1024 / 1024),
+                used: Math.round(+disk.used / 1024 / 1024 / 1024)
+            })),
+            disksIO: {
+                rbps: fsStats.rx_sec,
+                wbps: fsStats.wx_sec,
+                tbps: fsStats.tx_sec
+            },
+            network: network.map(iface => ({
+                iface: iface.iface,
+                bps: iface.tx_sec
+            })),
+            ...(ctx.request.headers['init'] && { meta: meta })
+        };
+    } else {
+        ctx.body = {
+            iterID: ''
+        };
+    }
 });
 
 const poll = async () => {
+    iterID = cuid();
     cpuTemp = await SI.cpuTemperature();
     cpuLoad = await SI.currentLoad();
     mem = await SI.mem();
     disk = await SI.fsSize();
     processes = await SI.processes();
-    disksIO = await SI.disksIO();
+    fsStats = await SI.fsStats();
     network = await SI.networkStats();
 };
 
@@ -66,7 +87,7 @@ const poll = async () => {
     cpu = await SI.cpu();
     os = await SI.osInfo();
 
-    setInterval(poll, 5000);
+    setInterval(poll, pollInterval);
 
-    app.listen(3000);
+    app.listen(port);
 })();
